673f7e00e778cd5ba232b8ea9bb1150bfba9fa4b
Niteesh7 <61003888+Niteesh7@users.noreply.github.com>
2021-05-15 07:32:05 +0530

    Create Java Static Initializer Block.java

---
+++ Java Static Initializer Block.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static int B;
+static int H;
+static boolean flag= true;
+static{
+    Scanner sc=new Scanner(System.in);
+    B=sc.nextInt();
+    H=sc.nextInt();
+    try{
+        if(B<=0||H<=0)
+        {
+            flag=false;
+            throw new  Exception ("Breadth and height must be positive");
+        }
+    }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            System.exit(0);
+        }
+   
+    
+}
+
+
+public static void main(String[] args){
